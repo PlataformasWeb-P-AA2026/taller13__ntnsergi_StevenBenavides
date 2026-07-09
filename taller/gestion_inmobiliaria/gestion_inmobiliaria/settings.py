795242b2125a39b4a684aebb333c751837895be9
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-07-09 12:53:03 -0500

    Final

--- taller/gestion_inmobiliaria/gestion_inmobiliaria/settings.py
+++ taller/gestion_inmobiliaria/gestion_inmobiliaria/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'gestion_inmobiliaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inmobiliaria_db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5434',
     }
 }
 
